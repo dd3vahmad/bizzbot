3e55829806947a22e02cbf8aec9355d81e74aa16
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-08-20 23:08:01 +0100

    Fix: chatting home page.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -45,3 +45,10 @@ export const getSizeMultiplicationFactor = (size: SIZES): number => {
 
   return mf;
 };
+
+export const getGreeting = () => {
+  const hour = new Date().getHours();
+  if (hour < 12) return "Good Morning";
+  if (hour < 17) return "Good Afternoon";
+  return "Good Evening";
+};
